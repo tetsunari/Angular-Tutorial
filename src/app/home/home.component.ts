cbb0184728de5991fe18638b080edd56360eb7b7
tetsunari <mtst.tty556@gmail.com>
2024-02-13 15:55:41 +0900

    type, inputデコレータ

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +17,23 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-    <section>
+    <section class="results">
       <app-housing-location></app-housing-location>
     </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
