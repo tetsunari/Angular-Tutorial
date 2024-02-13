d95f9ad48240fec1f8f370fd3dc699269e055591
tetsunari <mtst.tty556@gmail.com>
2024-02-13 15:31:30 +0900

    create housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city"/>
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css'
