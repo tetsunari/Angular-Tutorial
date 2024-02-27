8557b986b6269d5781240557bcfcf5301b599750
tetsunari <mtst.tty556@gmail.com>
2024-02-27 17:18:40 +0900

    Add HTTP communication

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,8 +34,10 @@ housingService: HousingService = inject(HousingService);
 filteredLocationList: HousingLocation[] = [];
 
 constructor() {
-  this.housingLocationList = this.housingService.getAllHousingLocations();
-  this.filteredLocationList = this.housingLocationList;
+  this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+    this.housingLocationList = housingLocationList;
+    this.filteredLocationList = housingLocationList;
+  })
 }
 
 filterResults(text: string) {
